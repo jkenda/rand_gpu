676fbdd33a7ce0788314551fdc2d3054ba088b4d
Jakob Kenda <kenda.jakob@gmail.com>
2022-04-12 21:59:42 +0200

    improved kernel stringify, updated cl.hpp

--- tools/convert_kernel.py
+++ tools/convert_kernel.py
@@ -8,14 +8,18 @@ if len(sys.argv) < 2:
 dst = ""
 src = None
 
-with open(f"{sys.argv[1]}", "r") as f:
+with open(sys.argv[1], "r") as f:
     src = f.readlines()
 
-dst = '#include <string>\n static const std::string KERNEL_SOURCE =\n'
+dst = """
+#include <string>
+static const std::string KERNEL_SOURCE =
+"""
+
 for line in src:
     if not line.isspace():
         dst += f'"{line[:-1]}\\n"\n'
-dst = f'{dst[:-1]};'
+dst = f"{dst[:-1]};\n"
 
 with open("kernel.hpp", "w") as f:
     f.write(dst)
