545e55778a032bfb1bd7ed305d0c28f59e720522
Jakob Kenda <kenda.jakob@gmail.com>
2022-06-07 19:14:35 +0200

    improved kernel string conversion

--- tools/convert_kernel.py
+++ tools/convert_kernel.py
@@ -7,18 +7,36 @@ if len(sys.argv) < 2:
 
 dst = ""
 src = None
+multiline_comment = False
 
 with open(sys.argv[1], "r") as f:
     src = f.readlines()
 
-dst = """
-static const char *KERNEL_SOURCE =
-"""
+dst = "static const char *KERNEL_SOURCE =\n"
 
 for line in src:
-    if not line.isspace():
-        dst += f'"{line[:-1]}\\n"\n'
-dst = f"{dst[:-1]};\n"
+
+    if multiline_comment:
+        comment_end = line.find("*/")
+        if comment_end == -1: continue
+        else:
+            line = line[comment_end+2:]
+            multiline_comment = False
+
+    comment_start = line.find("//")
+    line = line[:comment_start]
+
+    comment_start = line.find("/*")
+    if comment_start != -1:
+        line = line[:comment_start]
+        multiline_comment = True
+
+    line = line.strip()
+
+    if line and not line.isspace():
+        dst += f'"{line}\\n"\n'
+
+dst = f"{dst};\n"
 
 with open("kernel.hpp", "w") as f:
     f.write(dst)
